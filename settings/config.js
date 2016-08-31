13f70dd491a34b94c802767b24c7f30a69c1cb10
sreenesh <sreenesh@qburst.com>
2016-08-31 15:29:55 +0530

    port 9000

--- settings/config.js
+++ settings/config.js
@@ -2,6 +2,6 @@ module.exports =
 {
 	'secret' : 'longobnoxiouspassphrase',
 	'dbPath' : '127.0.0.1:27017/' + process.env.OPENSHIFT_APP_NAME,
-	'port' : process.env.OPENSHIFT_NODEJS_PORT || 8080,
+	'port' : process.env.OPENSHIFT_NODEJS_PORT || 9000,
 	'server_ip_address' : process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 };
